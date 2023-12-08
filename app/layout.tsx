37116e3dc5c75b27c301b63ca60bae9707f97cc8
Daria <roseannee.kim@gmail.com>
2023-12-08 22:21:29 +0100

    perf: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,25 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  icons: 'favicon.ico',
+  openGraph: {
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
